ed338bba878d233131bc6fc14d84ecec085db9ae
SucioLoco <irelandrj@yahoo.com>
2022-12-14 16:48:26 -0500

    test

--- core/settings.py
+++ core/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'whitenoise'
     'rest_framework',
     'corsheaders',
     'django_filters',
